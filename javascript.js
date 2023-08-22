8b45a49e837dd1305916d78bde11ea83018ac260
R.Sathish kumar <115446419+Sathiskumarssf@users.noreply.github.com>
2023-08-22 23:16:19 +0530

    e-commerce website

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.getElementById("buy-1").onclick=function add(){
+  window.location.assign("https://www.w3schools.com")
+}
+document.getElementById("buy-2").onclick=function add(){
+  window.location.assign("https://www.w3schools.com")
+}
+document.getElementById("buy-3").onclick=function add(){
+  window.location.assign("https://www.w3schools.com")
+}
